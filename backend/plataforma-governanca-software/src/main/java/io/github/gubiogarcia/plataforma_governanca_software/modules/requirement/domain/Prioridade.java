0b03533f7039e415942e4285ec1764a9f2a197a9
GubioGarcia <gubiogarcia@gmail.com>
2026-05-02 19:56:57 -0300

    Implementação de status de requisitos e ajustes relacionados

--- backend/plataforma-governanca-software/src/main/java/io/github/gubiogarcia/plataforma_governanca_software/modules/requirement/domain/Prioridade.java
+++ backend/plataforma-governanca-software/src/main/java/io/github/gubiogarcia/plataforma_governanca_software/modules/requirement/domain/Prioridade.java
@@ -24,7 +24,6 @@ public class Prioridade {
 
     private String descricao;
 
+    @Column(unique = true)
     private Integer ordem;
-
-    private Boolean ativo;
 }
---
+++ backend/plataforma-governanca-software/src/main/java/io/github/gubiogarcia/plataforma_governanca_software/modules/requirement/repository/RequisitoRepository.java
@@ -0,0 +1,15 @@
+package io.github.gubiogarcia.plataforma_governanca_software.modules.requirement.repository;
+
+import io.github.gubiogarcia.plataforma_governanca_software.modules.requirement.domain.Requisito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface RequisitoRepository extends JpaRepository<Requisito, UUID> {
+    List<Requisito> findAllByProjetoId(UUID projetoId);
+    boolean existsByStatusId(UUID statusId);
+    boolean existsByPrioridadeId(UUID prioridadeId);
+}
