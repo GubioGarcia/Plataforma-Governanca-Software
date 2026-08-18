5ede9d8dec182a1fd69fb48e37abc12696d3e24f
GubioGarcia <gubiogarcia@gmail.com>
2026-08-18 20:15:40 -0300

    test: adiciona testes de integração dos services de organização, projeto, status e requisito

--- backend/plataforma-governanca-software/src/test/java/io/github/gubiogarcia/plataforma_governanca_software/PlataformaDeGovernancaDeProjetosDeSoftwareApplicationTests.java
+++ backend/plataforma-governanca-software/src/test/java/io/github/gubiogarcia/plataforma_governanca_software/PlataformaDeGovernancaDeProjetosDeSoftwareApplicationTests.java
@@ -2,9 +2,16 @@ package io.github.gubiogarcia.plataforma_governanca_software;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PlataformaDeGovernancaDeProjetosDeSoftwareApplicationTests {
+
+	@MockitoBean
+	JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
